5d9e1fa3c38f7abdb35b2d879d1550bf15d46ac6
Anton Laurell <antonlaurell@hotmail.com>
2025-11-11 16:11:59 +0100

    ++

---
+++ build-and-run.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+rm -rf build && cmake . -B build && cmake --build build && ./build/bin
---
+++ src/main.cpp
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <webgpu.h>
+
+typedef struct AdapterRequestState {
+    WGPUAdapter adapter = NULL;
+    bool request_ended = false;
+} AdapterRequestState;
+
+void print_adapter_info(WGPUAdapter adapter) {
+    // Adapter info
+    WGPUAdapterInfo info = {0};
+    if (wgpuAdapterGetInfo(adapter, &info) == WGPUStatus_Success) {
+        printf("=== Adapter Info ===\n");
+        printf("Vendor:       %.*s\n", (int)info.vendor.length, info.vendor.data);
+        printf("Architecture: %.*s\n", (int)info.architecture.length, info.architecture.data);
+        printf("Device:       %.*s\n", (int)info.device.length, info.device.data);
+        printf("Description:  %.*s\n", (int)info.description.length, info.description.data);
+        printf("Backend:      %d\n", info.backendType);
+        printf("Adapter Type: %d\n", info.adapterType);
+        printf("Vendor ID:    %u\n", info.vendorID);
+        printf("Device ID:    %u\n", info.deviceID);
+    }
+    wgpuAdapterInfoFreeMembers(info);
+
+    // Adapter features
+    WGPUSupportedFeatures features = {0};
+    wgpuAdapterGetFeatures(adapter, &features);
+    printf("\n=== Supported Features (%zu) ===\n", features.featureCount);
+    for (size_t i = 0; i < features.featureCount; ++i) {
+        printf("  - Feature #%zu: %d\n", i, features.features[i]);
+    }
+    wgpuSupportedFeaturesFreeMembers(features);
+
+    // Adapter limits
+    WGPULimits limits = {0};
+    if (wgpuAdapterGetLimits(adapter, &limits) == WGPUStatus_Success) {
+        printf("\n=== Adapter Limits ===\n");
+        printf("Max 2D Texture Dimension: %u\n", limits.maxTextureDimension2D);
+        printf("Max Bind Groups:          %u\n", limits.maxBindGroups);
+        printf("Max Uniform Buffer Size:  %llu\n", (unsigned long long)limits.maxUniformBufferBindingSize);
+        printf("Max Storage Buffer Size:  %llu\n", (unsigned long long)limits.maxStorageBufferBindingSize);
+        printf("Max Vertex Buffers:       %u\n", limits.maxVertexBuffers);
+        printf("Max Vertex Attributes:    %u\n", limits.maxVertexAttributes);
+        printf("Max Compute Workgroup X:  %u\n", limits.maxComputeWorkgroupSizeX);
+        printf("Max Compute Workgroup Y:  %u\n", limits.maxComputeWorkgroupSizeY);
+        printf("Max Compute Workgroup Z:  %u\n", limits.maxComputeWorkgroupSizeZ);
+    }
+    printf("========================\n");
+}
+
+void on_adapter_request_ended(
+        WGPURequestAdapterStatus status,
+        WGPUAdapter adapter,
+        WGPUStringView message,
+        void *userdata1,
+        void *userdata2)
+{
+    AdapterRequestState *state = (AdapterRequestState*)userdata1;
+    state->adapter = adapter;
+    state->request_ended = true;
+
+    if (status == WGPURequestAdapterStatus_Success) {
+        printf("Adapter request succeeded\n");
+        // print_adapter_info(adapter);
+    }
+    else {
+        fprintf(stderr,
+                "Adapter request failed: %.*s\n",
+                (int)message.length, message.data);
+    }
+}
+
+typedef struct DeviceRequestState {
+    WGPUDevice device = NULL;
+    bool request_ended = false;
+} DeviceRequestState;
+
+void print_device_info(WGPUDevice device) {
+    // --- Device features
+    WGPUSupportedFeatures dfeatures = (WGPUSupportedFeatures){0};
+    wgpuDeviceGetFeatures(device, &dfeatures);
+    printf("\n=== Device Features (%zu) ===\n", dfeatures.featureCount);
+    for (size_t i = 0; i < dfeatures.featureCount; ++i) {
+        printf("  - %d\n", dfeatures.features[i]); // WGPUFeatureName
+    }
+    wgpuSupportedFeaturesFreeMembers(dfeatures);
+
+    // --- Device limits
+    WGPULimits dlimits = (WGPULimits){0};
+    if (wgpuDeviceGetLimits(device, &dlimits) == WGPUStatus_Success) {
+        printf("\n=== Device Limits ===\n");
+        printf("Max 2D Texture Dimension: %u\n", dlimits.maxTextureDimension2D);
+        printf("Max Bind Groups:          %u\n", dlimits.maxBindGroups);
+        printf("Max Uniform Buffer Size:  %llu\n", (unsigned long long)dlimits.maxUniformBufferBindingSize);
+        printf("Max Storage Buffer Size:  %llu\n", (unsigned long long)dlimits.maxStorageBufferBindingSize);
+        printf("Max Vertex Buffers:       %u\n", dlimits.maxVertexBuffers);
+        printf("Max Vertex Attributes:    %u\n", dlimits.maxVertexAttributes);
+        printf("Max Compute WG Size X/Y/Z:%u/%u/%u\n",
+               dlimits.maxComputeWorkgroupSizeX,
+               dlimits.maxComputeWorkgroupSizeY,
+               dlimits.maxComputeWorkgroupSizeZ);
+    }
+    printf("========================\n");
+}
+
+void on_device_request_ended(
+    WGPURequestDeviceStatus status,
+    WGPUDevice device,
+    WGPUStringView message,
+    void *userdata1,
+    void *userdata2)
+{
+    DeviceRequestState *device_request_state = (DeviceRequestState*)userdata1;
+    device_request_state->device = device;
+    device_request_state->request_ended = true;
+    if (status == WGPURequestDeviceStatus_Success) {
+        printf("Device request succeeded\n");
+        // print_device_info(device);
+    }
+    else {
+        fprintf(stderr,
+                "Device request failed: %.*s\n",
+                (int)message.length, message.data);
+    }
+}
+
+
+WGPUDevice request_device(WGPUInstance instance)
+{
+    // get adapter
+    struct AdapterRequestState adapter_request_state;
+    WGPURequestAdapterCallbackInfo adapter_callback_info = {0};
+    adapter_callback_info.callback = on_adapter_request_ended;
+    adapter_callback_info.userdata1 = &adapter_request_state;
+    WGPURequestAdapterOptions adapter_request_options = {0};
+    adapter_request_options.nextInChain = NULL;
+    adapter_request_options.featureLevel = WGPUFeatureLevel_Core;
+    wgpuInstanceRequestAdapter(instance, &adapter_request_options, adapter_callback_info);
+    while(!adapter_request_state.request_ended) {
+        wgpuInstanceProcessEvents(instance);
+    }
+
+    // get device
+    DeviceRequestState device_request_state;
+    WGPUDeviceDescriptor device_desc = {0};
+    device_desc.nextInChain = NULL;
+    device_desc.defaultQueue.nextInChain = NULL;
+    device_desc.deviceLostCallbackInfo = {0}; // TODO: device lost callback
+    WGPURequestDeviceCallbackInfo device_callback_info = {0};
+    device_callback_info.nextInChain = NULL;
+    device_callback_info.callback = on_device_request_ended;
+    device_callback_info.userdata1 = &device_request_state;
+    wgpuAdapterRequestDevice(adapter_request_state.adapter, &device_desc, device_callback_info);
+    while (!device_request_state.request_ended) { // TODO: prettier busywait
+        wgpuInstanceProcessEvents(instance);
+    }
+    wgpuAdapterRelease(adapter_request_state.adapter);
+    return device_request_state.device;
+}
+
+int main() {
+    WGPUInstanceDescriptor instance_desc = {0};
+    instance_desc.nextInChain= NULL;
+    WGPUInstance instance = wgpuCreateInstance(&instance_desc);
+    WGPUDevice device = request_device(instance);
+
+    WGPUQueue queue = wgpuDeviceGetQueue(device);
+
+    WGPUCommandEncoderDescriptor encoder_desc = {};
+    encoder_desc.nextInChain = NULL;
+    WGPUCommandEncoder encoder = wgpuDeviceCreateCommandEncoder(device, &encoder_desc);
+
+    // release
+    wgpuDeviceRelease(device);
+    wgpuInstanceRelease(instance);
+}
+
